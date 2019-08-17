b9a312d16ce3b90f3f0ea32b11a26653c25ca340
Samuel Possamai <possamai.sam@gmail.com>
2019-08-17 17:05:51 -0300

    Using database to save ideas

--- app.py
+++ app.py
@@ -9,25 +9,6 @@ import sqlalchemy
 
 import hashlib
 
-SAMPLE_IDEAS = [(0, 'User1', 'Ideia1',
-                 '''
-1word word word word word word word word word word word word word word word word word word word word word
-    2word word word word word word word word word word word word word word word word word word word word word
-3word word word word word word word word word word word word word word word word word word word word word
-                 '''),
-                (1, 'User2', 'Ideia2',
-                 ('word word word word word word word word word word word word word word word '
-                  'word word word word word word word word word word word word word word word '
-                  'word word word word word word word word word word word word word word word '
-                  'word word word word word word word word word word word word word word word '
-                  'word word word word word word word word word word word word word word word '
-                  'word word word word word word word word word word word word word word word '
-                  'word word word word word word word word word word word word word word word '
-                  'word word word word word word word word word word word word word word word\n'
-                  'word word word word word word word word word word word word word word word ')),
-                (2, 'User3', 'Ideia3', ''),
-                (3, 'User4', 'Ideia4', '')]
-
 app = Flask(__name__)
 sql_engine = sqlalchemy.create_engine('sqlite:///app.sqlite')
 sql_metadata = sqlalchemy.MetaData()
@@ -37,10 +18,13 @@ users_table = sqlalchemy.Table('users', sql_metadata,
                                sqlalchemy.Column('name', sqlalchemy.VARCHAR(length=64), unique=True),
                                sqlalchemy.Column('password_hash', sqlalchemy.VARCHAR(length=256)))
 
-def setup_database():
+ideas_table = sqlalchemy.Table('ideas', sql_metadata,
+                               sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+                               sqlalchemy.Column('user', sqlalchemy.Integer),
+                               sqlalchemy.Column('title', sqlalchemy.VARCHAR(length=64), unique=True),
+                               sqlalchemy.Column('content', sqlalchemy.Text()))
 
-    if not sql_engine.dialect.has_table(sql_engine, 'users'):
-        sql_metadata.create_all(sql_engine)
+sql_metadata.create_all(sql_engine)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
@@ -68,6 +52,7 @@ def login_page():
             return render_template('login.html', error=True)
 
         session['user'] = username
+        session['user_id'] = user_info[result.keys().index('id')]
 
         return redirect(session.get('login_return_url') or url_for('home_page'))
 
@@ -86,15 +71,44 @@ def ideas_page():
         session['login_return_url'] = url_for('ideas_page')
         return redirect(url_for('login_page'))
 
-    return render_template('ideas.html', ideas=SAMPLE_IDEAS)
+    sql_connection = sql_engine.connect()
+
+    query = users_table.select()
+
+    result = sql_connection.execute(query)
+
+    users_id_index = result.keys().index('id')
+    users_name_index = result.keys().index('name')
+    users = {user_info[users_id_index]: user_info[users_name_index] for user_info in result}
+
+    query = ideas_table.select().order_by(ideas_table.c.id.desc())
+
+    result = sql_connection.execute(query)
+
+    ideas_table_columns = result.keys()
+
+    table_indexes = (ideas_table_columns.index('id'),
+                     ideas_table_columns.index('user'),
+                     ideas_table_columns.index('title'),
+                     ideas_table_columns.index('content'))
+
+    ideas = tuple(tuple((idea[index] if i != 1 else users.get(idea[index])
+                         for i, index in enumerate(table_indexes))) for idea in result)
+
+    return render_template('ideas.html', ideas=ideas)
 
 @app.route('/ideas/new', methods=['GET', 'POST'])
 def create_idea_page():
 
     if request.method == 'POST':
 
-        SAMPLE_IDEAS.append((SAMPLE_IDEAS[-1][0] + 1, session.get('user'),
-                             request.form['idea-title'], request.form['idea-content']))
+        sql_connection = sql_engine.connect()
+
+        query = ideas_table.insert().values(user=session.get('user_id'),
+                                            title=request.form['idea-title'],
+                                            content=request.form['idea-content'])
+
+        sql_connection.execute(query)
 
         return redirect(url_for('ideas_page'))
 
@@ -108,20 +122,42 @@ def create_idea_page():
 @app.route('/ideas/<int:idea_id>')
 def single_idea_page(idea_id):
 
-    for idea in SAMPLE_IDEAS:
+    sql_connection = sql_engine.connect()
 
-        if idea[0] == idea_id:
-            return render_template('singleidea.html', idea=idea)
+    query = ideas_table.select().where(sqlalchemy.text(f'id = {idea_id}'))
 
-    return 'Invalid', 401
+    result = sql_connection.execute(query)
+
+    idea_info = result.first()
+    if idea_info is None:
+        return 'Invalid', 401
+
+    ideas_table_columns = result.keys()
+    table_indexes = (ideas_table_columns.index('id'),
+                     ideas_table_columns.index('user'),
+                     ideas_table_columns.index('title'),
+                     ideas_table_columns.index('content'))
+
+    query = users_table.select().where(sqlalchemy.text(f'id = {idea_info[table_indexes[1]]}'))
+
+    result = sql_connection.execute(query)
+
+    user_info = result.first()
+
+    if user_info is None:
+        return 'Invalid', 401
+
+    author = user_info[result.keys().index('name')]
+
+    idea = tuple((idea_info[index] if i != 1 else author for i, index in enumerate(table_indexes)))
+
+    return render_template('singleidea.html', idea=idea)
 
 @app.route('/')
 def home_page():
     return render_template('home.html')
 
 if __name__ == '__main__':
-
-    setup_database()
 
     app.permanent_session_lifetime =  timedelta(minutes=20)
     app.secret_key = secrets.token_bytes(8)
